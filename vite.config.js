06c0fea48addaa023f60025eb44d7a2807b9de9e
Northwalker <northwalker28@gmail.com>
2024-02-03 16:55:05 +0800

    Vitest 0203

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'luke-actions-demo',
+  // base:'luke-actions-demo',
   plugins: [react()],
+  test:{
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      include: ['src/js/**/*.js'],
+    }
+  }
 })
