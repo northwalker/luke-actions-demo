3e42df49feb1f1e5d654cdf7bbd2604b30d961c1
Northwalker <northwalker28@gmail.com>
2024-02-28 18:49:00 +0800

    Change to exec sync

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import * as child from 'child_process';
+
+const commitHash = child
+  .execSync('git rev-parse --short HEAD')
+  .toString()
+  ?.replace(/\n/g, '');
+console.log('commitHash', commitHash);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // eslint-disable-next-line no-undef
   base: process.env.NODE_ENV === 'production' ? 'luke-actions-demo' : '',
   plugins: [react()],
+  define: {
+    'import.meta.env.__COMMIT_HASH__': JSON.stringify(commitHash),
+  },
 });
